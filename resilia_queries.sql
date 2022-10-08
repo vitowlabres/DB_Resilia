28eaa07251c30c754daa8aa8c0c0f6040f9cbcdc
vitowlabres <vitorlabres12@gmail.com>
2022-10-08 10:37:01 -0300

    inserção da query de estudantes por curso

--- resilia_queries.sql
+++ resilia_queries.sql
@@ -2,3 +2,14 @@ use Resilia;
 
 #Quantos alunos no total?
 SELECT COUNT(id_aluno) Alunos from Alunos;
+
+#Todos os estudantes e seus respectivos cursos
+SELECT 
+    a.nome aluno, c.curso, e.nome empresa_parceira, a.id_curso
+FROM
+    Alunos a
+        INNER JOIN
+    Cursos c USING (id_curso)
+        INNER JOIN
+    EmpresasParceiras e USING (id_emp_parceira)
+ORDER BY aluno;
